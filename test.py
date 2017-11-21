0fc2da1c416aad7d8041641c934060763cbfd717
Alexander Lipin <lipinalexander@gmail.com>
2017-11-21 18:36:13 +0300

    modifying firstbranch test.py

--- test.py
+++ test.py
@@ -2,5 +2,7 @@
 
 print('Enter your name: ')
 user_name = input()
+print('Enter your age: ')
+user_age = input()
 
-print('Your name is', user_name)
+print('Your name is', user_name, 'you are', str(user_age), 'age old')
